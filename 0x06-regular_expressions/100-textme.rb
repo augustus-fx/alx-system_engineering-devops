f97f7d7e141fe2fb41083abd486eacaef8aa7eb2
augustus-fx <agbolanaugustine@gmail.com>
2023-03-16 00:50:41 +0000

    Corre...

--- 0x06-regular_expressions/100-textme.rb
+++ 0x06-regular_expressions/100-textme.rb
@@ -1,6 +1,4 @@
 #!/usr/bin/env ruby
-
-from = ARGV[0].scan(/from:(.*?)\]/)
-to = ARGV[0].scan(/to:(.*?)\]/)
-flags = ARGV[0].scan(/flags:(.*?)\]/)
-puts [from, to, flags].join(',')
+Format = ARGV[0].scan(/from:(.\w+)|to:(.\w+)|flags:([0-9:-]+)/)
+List = [Format[0].compact, Format[1].compact, Format[2].compact]
+puts List.join(',')
